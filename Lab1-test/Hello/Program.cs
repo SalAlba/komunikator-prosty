94172248a6292c723677b93214004b3d05b7b6ee
SalAlba <saljan93@live.com>
2017-04-01 14:44:55 +0200

    First commit

---
+++ Lab1-test/Hello/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Hello
+{
+    class Program
+    {
+       // private static TcpListener tcpLsn;
+        private static Socket socket;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conect client :)");
+            conect();
+            Console.WriteLine("Client sending");
+            send("hi there I'm Salem :)");
+            Console.ReadLine();
+        }
+
+        private static void conect() {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress hostadd = IPAddress.Parse("127.0.0.1");
+            int port = 2222;
+
+            IPEndPoint ipEndPoint = new IPEndPoint(hostadd,port);
+            socket.Connect(ipEndPoint);
+        }
+
+
+        private static void send(string msg) {
+            Byte[] byteData = Encoding.ASCII.GetBytes(msg.ToCharArray());
+            socket.Send(byteData, byteData.Length, 0);
+        }
+
+
+    }
+}
---
+++ Lab1-test/Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Server
+{
+    class Program
+    {
+        private static TcpListener tcpLsn;
+        private static Socket socket;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start server ...");
+            serwuj();
+
+            Console.ReadLine();
+        }
+
+        private static void serwuj() {
+            tcpLsn = new TcpListener(IPAddress.Parse("127.0.0.1"), 2222);
+            tcpLsn.Start();  // START *******************
+            socket = tcpLsn.AcceptSocket(); // fun. blokująca az do momentu nadejszcia polacien
+
+            Byte[] receiveByte = new Byte[100];
+            int ret = socket.Receive(receiveByte, receiveByte.Length, 0);
+            string tmp = null;
+            tmp = System.Text.Encoding.ASCII.GetString(receiveByte);
+            if(tmp.Length > 0)
+            {
+                Console.WriteLine("Odebrałem komunikat ");
+                Console.WriteLine(tmp);
+            }
+
+            tcpLsn.Stop(); // STOP ******************
+        }
+    }
+}
